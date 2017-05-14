a59e830444227eecd997b16e6ad15a79ea7f6b9e
Shahid <tallstoat@gmail.com>
2017-05-14 12:28:48 +0530

    Add test for data types

---
+++ datatype_test.go
@@ -0,0 +1,23 @@
+package pbparser
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDataType(t *testing.T) {
+	var tests = []struct {
+		s string
+	}{
+		{s: "any"},
+		{s: "int32"},
+	}
+
+	// initialize once...
+	initScalarDataType()
+
+	for _, tt := range tests {
+		x := GetScalarType(tt.s)
+		fmt.Printf("Scalar Type: %v for input string: %v \n", x, tt.s)
+	}
+}
