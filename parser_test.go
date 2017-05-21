0724b286c0b8e075868a15de781838ef780f1458
Shahid <tallstoat@gmail.com>
2017-05-21 12:52:49 +0530

    Clean the parser tests

--- parser_test.go
+++ parser_test.go
@@ -11,12 +11,16 @@ func TestParseFile(t *testing.T) {
 	var tests = []struct {
 		file string
 	}{
+		{file: "./resources/enum.proto"},
 		{file: "./resources/service.proto"},
 	}
 
 	for i, tt := range tests {
-		fmt.Printf("Running test: %v \n", i)
+		fmt.Printf("Running test: %v \n\n", i)
 		fmt.Printf("Parsing file: %v \n", tt.file)
+
+		tab := indent(2)
+		tab2 := indent(4)
 
 		pf, err := pbparser.ParseFile(tt.file)
 		if err != nil {
@@ -25,116 +29,127 @@ func TestParseFile(t *testing.T) {
 
 		fmt.Println("Syntax: " + pf.Syntax)
 		fmt.Println("PackageName: " + pf.PackageName)
-
 		for _, d := range pf.Dependencies {
-			fmt.Println()
 			fmt.Println("Dependency: " + d)
 		}
-
 		for _, d := range pf.PublicDependencies {
-			fmt.Println()
 			fmt.Println("PublicDependency: " + d)
 		}
 
 		for _, m := range pf.Messages {
-			fmt.Println()
 			fmt.Println("Message: " + m.Name)
 			fmt.Println("QualifiedName: " + m.QualifiedName)
-			fmt.Println("Doc: " + m.Documentation)
+			if m.Documentation != "" {
+				fmt.Println("Doc: " + m.Documentation)
+			}
 			for _, f := range m.Fields {
-				fmt.Println()
-				fmt.Println("Field: " + f.Name)
+				fmt.Println(tab + "Field: " + f.Name)
 				if f.Label != "" {
-					fmt.Println("Label: " + f.Label)
+					fmt.Println(tab + "Label: " + f.Label)
 				}
-				fmt.Printf("Type: %v\n", f.Type)
-				fmt.Printf("Tag: %v\n", f.Tag)
-				fmt.Println("Doc: " + f.Documentation)
+				fmt.Printf("%vType: %v\n", tab, f.Type)
+				fmt.Printf("%vTag: %v\n", tab, f.Tag)
+				if f.Documentation != "" {
+					fmt.Println(tab + "Doc: " + f.Documentation)
+				}
 			}
 			for _, oo := range m.OneOfs {
-				fmt.Println()
-				fmt.Println("OneOff: " + oo.Name)
-				fmt.Println("Doc: " + oo.Documentation)
+				fmt.Println(tab + "OneOff: " + oo.Name)
+				if oo.Documentation != "" {
+					fmt.Println(tab + "Doc: " + oo.Documentation)
+				}
 				for _, f := range oo.Fields {
-					fmt.Println()
-					fmt.Println("OneOff Field: " + f.Name)
+					fmt.Println(tab2 + "OneOff Field: " + f.Name)
 					if f.Label != "" {
-						fmt.Println("Label: " + f.Label)
+						fmt.Println(tab2 + "Label: " + f.Label)
 					}
-					fmt.Printf("Type: %v\n", f.Type)
-					fmt.Printf("Tag: %v\n", f.Tag)
-					fmt.Println("Doc: " + f.Documentation)
+					fmt.Printf("%vType: %v\n", tab2, f.Type)
+					fmt.Printf("%vTag: %v\n", tab2, f.Tag)
+					if f.Documentation != "" {
+						fmt.Println(tab2 + "Doc: " + f.Documentation)
+					}
 				}
 			}
 			for _, xe := range m.Extensions {
-				fmt.Println()
-				fmt.Printf("Extensions Start: %v\n", xe.Start)
-				fmt.Printf("Extensions End: %v\n", xe.End)
-				fmt.Println("Extensions Doc: " + xe.Documentation)
+				fmt.Printf("%vExtensions:: Start: %v End: %v\n", tab, xe.Start, xe.End)
+				if xe.Documentation != "" {
+					fmt.Println(tab + "Extensions:: Doc: " + xe.Documentation)
+				}
 			}
 			for _, rn := range m.ReservedNames {
-				fmt.Println()
-				fmt.Printf("Reserved Name: " + rn)
+				fmt.Println(tab + "Reserved Name: " + rn)
 			}
 			for _, rr := range m.ReservedRanges {
-				fmt.Println()
-				fmt.Printf("Reserved Range: %v to %v\n", rr.Start, rr.End)
-				fmt.Println("Reserved Range Doc: " + rr.Documentation)
+				fmt.Printf("%vReserved Range:: Start: %v to End: %v\n", tab, rr.Start, rr.End)
+				if rr.Documentation != "" {
+					fmt.Println(tab + "Reserved Range:: Doc: " + rr.Documentation)
+				}
 			}
 		}
 
 		for _, ed := range pf.ExtendDeclarations {
-			fmt.Println()
 			fmt.Println("Extend: " + ed.Name)
 			fmt.Println("QualifiedName: " + ed.QualifiedName)
-			fmt.Println("Doc: " + ed.Documentation)
+			if ed.Documentation != "" {
+				fmt.Println("Doc: " + ed.Documentation)
+			}
 			for _, f := range ed.Fields {
-				fmt.Println()
-				fmt.Println("Field: " + f.Name)
+				fmt.Println(tab + "Field: " + f.Name)
 				if f.Label != "" {
-					fmt.Println("Label: " + f.Label)
+					fmt.Println(tab + "Label: " + f.Label)
 				}
-				fmt.Printf("Type: %v\n", f.Type)
-				fmt.Printf("Tag: %v\n", f.Tag)
-				fmt.Println("Doc: " + f.Documentation)
+				fmt.Printf("%vType: %v\n", tab, f.Type)
+				fmt.Printf("%vTag: %v\n", tab, f.Tag)
+				if f.Documentation != "" {
+					fmt.Println(tab + "Doc: " + f.Documentation)
+				}
 			}
 		}
 
 		for _, s := range pf.Services {
-			fmt.Println()
 			fmt.Println("Service: " + s.Name)
 			fmt.Println("QualifiedName: " + s.QualifiedName)
-			fmt.Println("Doc: " + s.Documentation)
+			if s.Documentation != "" {
+				fmt.Println("Doc: " + s.Documentation)
+			}
 			for _, rpc := range s.RPCs {
-				fmt.Println()
-				fmt.Println("RPC: " + rpc.Name)
-				fmt.Println("Doc: " + rpc.Documentation)
+				fmt.Println(tab + "RPC: " + rpc.Name)
+				if rpc.Documentation != "" {
+					fmt.Println(tab + "Doc: " + rpc.Documentation)
+				}
 				if rpc.RequestType.IsStream() {
-					fmt.Println("RequestType: stream " + rpc.RequestType.Name())
+					fmt.Println(tab + "RequestType: stream " + rpc.RequestType.Name())
 				} else {
-					fmt.Println("RequestType: " + rpc.RequestType.Name())
+					fmt.Println(tab + "RequestType: " + rpc.RequestType.Name())
 				}
 				if rpc.ResponseType.IsStream() {
-					fmt.Println("ResponseType: stream " + rpc.ResponseType.Name())
+					fmt.Println(tab + "ResponseType: stream " + rpc.ResponseType.Name())
 				} else {
-					fmt.Println("ResponseType: " + rpc.ResponseType.Name())
+					fmt.Println(tab + "ResponseType: " + rpc.ResponseType.Name())
 				}
 			}
 		}
 
 		for _, en := range pf.Enums {
-			fmt.Println()
 			fmt.Println("Enum: " + en.Name)
 			fmt.Println("QualifiedName: " + en.QualifiedName)
-			fmt.Println("Doc: " + en.Documentation)
+			if en.Documentation != "" {
+				fmt.Println("Doc: " + en.Documentation)
+			}
 			for _, enc := range en.EnumConstants {
-				fmt.Println("Name: " + enc.Name)
-				fmt.Printf("Tag: %v \n", enc.Tag)
+				fmt.Printf("%vName: %v Tag: %v\n", tab, enc.Name, enc.Tag)
 			}
 		}
-
-		fmt.Printf("\nFinished test: %v \n", i)
+		fmt.Printf("\nFinished test: %v \n\n", i)
 	}
 
 	fmt.Println("done")
+}
+
+func indent(i int) string {
+	s := " "
+	for j := 0; j < i; j++ {
+		s += " "
+	}
+	return s
 }
