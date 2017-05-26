48a04493c1f150100b1ef3afe27afdd8f1d46a39
Shahid <tallstoat@gmail.com>
2017-05-26 21:34:25 +0530

    Code consolidation - Part III

--- verifier.go
+++ verifier.go
@@ -14,28 +14,29 @@ func verify(filePath string, pf *ProtoFile) error {
 	m := make(map[string]ProtoFile)
 
 	// parse the dependencies...
-	for _, d := range pf.Dependencies {
-		dependencyPath := dir + string(filepath.Separator) + d
-		dpf, err := parseFile(dependencyPath)
-		if err != nil {
-			msg := fmt.Sprintf("Unable to parse dependency %v (at: %v) of file: %v. Reason:: %v", d, dependencyPath, filePath, err.Error())
-			return errors.New(msg)
-		}
-		m[d] = dpf
+	if err := parseDependencies(dir, filePath, pf.Dependencies, m); err != nil {
+		return err
 	}
 
 	// parse the public dependencies...
-	for _, pd := range pf.PublicDependencies {
-		publicDependencyPath := dir + string(filepath.Separator) + pd
-		pdpf, err := parseFile(publicDependencyPath)
-		if err != nil {
-			msg := fmt.Sprintf("Unable to parse public dependency %v (at: %v) of file: %v. Reason:: %v", pd, publicDependencyPath, filePath, err.Error())
-			return errors.New(msg)
-		}
-		m[pd] = pdpf
+	if err := parseDependencies(dir, filePath, pf.PublicDependencies, m); err != nil {
+		return err
 	}
 
 	// TODO: add more checks here
 
+	return nil
+}
+
+func parseDependencies(dir string, fpath string, dependencies []string, m map[string]ProtoFile) error {
+	for _, d := range dependencies {
+		dependencyPath := dir + string(filepath.Separator) + d
+		dpf, err := parseFile(dependencyPath)
+		if err != nil {
+			msg := fmt.Sprintf("Unable to parse dependency %v (at: %v) of file: %v. Reason:: %v", d, dependencyPath, fpath, err.Error())
+			return errors.New(msg)
+		}
+		m[d] = dpf
+	}
 	return nil
 }
